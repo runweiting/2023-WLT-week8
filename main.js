ffd463e737aad492c2cf46f3216140dc2329268c
runweiting <whatisfuntime@gmail.com>
2023-09-09 12:17:33 +0800

    助教回饋3:swiper，看了三四個同學的code還是找不出原因＠＠

--- main.js
+++ main.js
@@ -1,33 +1,7 @@
 // bootstrap5
 import './assets/scss/all.scss';
+import './assets/js/swiper';
 import 'bootstrap/dist/js/bootstrap.min.js';
-
-// swiper
-import Swiper from 'swiper/bundle';
-import 'swiper/css/bundle';
-// init swiper
-const swiper = new Swiper('.swiper',{
-    spaceBetween: 24,
-    slidePreview: 3, // 看到幾張slides
-    scrollbar: {
-        el: ".swiper-scrollbar",
-        draggable: true, // 可拖動
-    },
-    mousewheel: true, // 滑鼠中鍵滾輪可滑動
-    // Responsive breakpoints
-    breakpoints: {
-        // >= 375px
-        375: {
-            direction: 'vertical',
-        },
-        // >= 1920px
-        376: {
-            direction: 'horizontal',
-        },
-    },
-});
-
-
 
 
 console.log("Hello world!");
